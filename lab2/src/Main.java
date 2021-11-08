e6edf07e890613f741030c9c0f0c2fee572f8a2f
alexclapou <alex.clapou@yahoo.com>
2021-11-08 11:36:55 +0200

    fa

--- lab2/src/Main.java
+++ lab2/src/Main.java
@@ -1,12 +1,21 @@
+import fa.FiniteAutomata;
 import scanner.LexicalAnalyzer;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args){
-        LexicalAnalyzer sc = new LexicalAnalyzer();
-        sc.Scan("p1");
+        //LexicalAnalyzer sc = new LexicalAnalyzer();
+        //sc.Scan("p1");
+        FiniteAutomata fa = new FiniteAutomata();
+        try {
+            fa.runMenu();
+        }
+        catch (IOException e){
+            System.out.println(e.getMessage());
+        }
     }
 }
